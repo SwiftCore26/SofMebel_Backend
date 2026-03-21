8f04ffa932387c3b7e8f3006efa5f96d89220bbc
sarvar-devx <sarvarbekdavranbekov@gmail.com>
2026-03-21 21:53:26 +0500

    Category admin

--- apps/admin.py
+++ apps/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.contrib.admin import TabularInline
+from django.contrib.admin import TabularInline, ModelAdmin
 
-from apps.models import ProductImage, Product
+from apps.models import ProductImage, Product, Category
 
 
 class ProductImageInline(TabularInline):
@@ -10,9 +10,20 @@ class ProductImageInline(TabularInline):
 
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
     search_fields = ('name', 'category__name')
     list_filter = ('category', 'name')
     ordering = ('id',)
+    readonly_fields = ('id', 'slug',)
     inlines = (ProductImageInline,)
+    list_display_links = ('id', 'name')
+
+
+@admin.register(Category)
+class CategoryAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    search_fields = ('name',)
+    ordering = ('id',)
+    readonly_fields = ('id', 'slug',)
+    list_display_links = ('id', 'name')
