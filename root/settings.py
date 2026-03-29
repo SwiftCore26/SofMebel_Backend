e9dbcd21ccb7adb91003156077907dac2c70fd7a
krv006 <rvkamronbek@gmail.com>
2026-03-29 20:26:13 +0500

    change

--- root/settings.py
+++ root/settings.py
@@ -20,11 +20,11 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "https://sof-mebel.netlify.app",
+    "https://www.thesofmebel.uz",
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:3002",
     "http://localhost:3003",
-
 ]
 
 CORS_ALLOW_HEADERS = [
